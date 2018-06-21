9668d7f75f389f323842ebede9037e5e4d603eb4
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-06-21 09:48:44 -0700

    update mongo test

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,20 @@
 extern crate bson;
 extern crate mongodb;
 
+use std::collections::HashMap;
 use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
-static mut db : ThreadedDatabase;
+static mut dbMap : HashMap.new();
 
 #[no_mangle]
 pub extern fn init_db() {
     let client = Client::with_uri("mongodb://HDF5MetadataTest_admin:ekekek19294jdwss2k@mongodb03.nersc.gov/HDF5MetadataTest")
     .expect("Failed on connection");
-    db = client.db("HDF5MetadataTest");
+    let db = client.db("HDF5MetadataTest");
     db.auth("HDF5MetadataTest_admin","ekekek19294jdwss2k").unwrap();
+    dbMap.insert(String::from("db"), db);
 }
 
 #[no_mangle]
@@ -21,6 +23,8 @@ pub extern fn random_test() {
     for n in (1..4).rev() {
         println!("Hello,my ssss world! {}", n);
     }
+    let db_key = String::from("db");
+    let db = dbMap.get(&db_key);
     let coll = db.collection("abcde");
 
     coll.insert_one(doc!{ "title" => "Back to the Future" }, None).unwrap();
