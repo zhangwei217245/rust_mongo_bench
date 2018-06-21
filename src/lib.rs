9e099b5e868c22fe6ecc2ba458771c786b26f1bf
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-06-21 10:14:19 -0700

    update mongo test

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,29 @@
 #[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
+#[macro_use]
+extern crate lazy_static;
 
 use std::collections::HashMap;
 use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
-static mut dbMap ;
+lazy_static! {
+    static ref DBMAP: HashMap<u32, &'static str> = {
+        let mut dbMap = HashMap::new();
+        let client = Client::with_uri("mongodb://HDF5MetadataTest_admin:ekekek19294jdwss2k@mongodb03.nersc.gov/HDF5MetadataTest")
+        .expect("Failed on connection");
+        let db = client.db("HDF5MetadataTest");
+        db.auth("HDF5MetadataTest_admin","ekekek19294jdwss2k").unwrap();
+        dbMap.insert(String::from("db"), db);
+        dbMap
+    };
+}
 
 #[no_mangle]
 pub extern fn init_db() {
-    dbMap = HashMap::new();
-    let client = Client::with_uri("mongodb://HDF5MetadataTest_admin:ekekek19294jdwss2k@mongodb03.nersc.gov/HDF5MetadataTest")
-    .expect("Failed on connection");
-    let db = client.db("HDF5MetadataTest");
-    db.auth("HDF5MetadataTest_admin","ekekek19294jdwss2k").unwrap();
-    dbMap.insert(String::from("db"), db);
+    DBMAP.get(String::from("db")).unwrap();
 }
 
 #[no_mangle]
@@ -25,7 +32,7 @@ pub extern fn random_test() {
         println!("Hello,my ssss world! {}", n);
     }
     let db_key = String::from("db");
-    let db = dbMap.get(&db_key);
+    let db = DBMAP.get(&db_key);
     let coll = db.collection("abcde");
 
     coll.insert_one(doc!{ "title" => "Back to the Future" }, None).unwrap();
