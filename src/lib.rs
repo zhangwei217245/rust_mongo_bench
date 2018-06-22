50192c93bdcd982e01462d5a965cc784655e2dbb
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-06-22 09:44:20 -0700

    update mongo test

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,60 @@
 #[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
+extern crate serde_json;
+
+
 #[macro_use]
 extern crate lazy_static;
+extern crate libc;
 
-// use std::collections::HashMap;
-// use std::string::String;
+use std::c_str::CString;
+use std::ffi::CStr;
+use std::str;
+use libc::c_char;
 use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::{Database, ThreadedDatabase};
+use mongodb::coll::Collection;
+use serde_json::{Value, Error};
+
+// lazy_static! {
+//     static ref MONGO_DB: Database = {
+//         //let mut dbMap = HashMap::new();
+//         let client = Client::with_uri("mongodb://HDF5MetadataTest_admin:ekekek19294jdwss2k@mongodb03.nersc.gov/HDF5MetadataTest")
+//         .expect("Failed on connection");
+//         let db = client.db("HDF5MetadataTest");
+//         db.auth("HDF5MetadataTest_admin","ekekek19294jdwss2k").unwrap();
+//         //dbMap.insert(String::from("db"), &db);
+//         db
+//     };
+// }
 
 lazy_static! {
-    static ref MONGO_DB: Database = {
+    static ref MONGO_COLL: Collection = {
         //let mut dbMap = HashMap::new();
         let client = Client::with_uri("mongodb://HDF5MetadataTest_admin:ekekek19294jdwss2k@mongodb03.nersc.gov/HDF5MetadataTest")
         .expect("Failed on connection");
         let db = client.db("HDF5MetadataTest");
         db.auth("HDF5MetadataTest_admin","ekekek19294jdwss2k").unwrap();
-        //dbMap.insert(String::from("db"), &db);
-        db
+        db.collection("abcde");
     };
 }
 
 #[no_mangle]
-pub extern fn init_db() {
-    let _coll = MONGO_DB.collection("abcde");
+pub extern fn init_db -> i64() {
+    let _coll = MONGO_COLL;
+    _coll.count(doc!{}, None).ok().expect("failed to initiate collection.");
+}
+
+#[no_mangle]
+pub extern "C" fn importing_json_doc_to_db (json_str: *const c_char) -> i32 {
+    let c_str = unsafe {
+        assert!(!s.is_null());
+        CStr::from_ptr(c_char);
+    };
+    let r_str = c_str.to_str().unwrap();
+    r_str.chars().count()
 }
 
 #[no_mangle]
