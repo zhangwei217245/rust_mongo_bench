3de146ed4783b26c0bfe45d863494096d1157858
Wei Zhang <X-Spirit.zhang@ttu.edu>
2018-09-27 14:56:41 -0500

    try insert many

--- src/lib.rs
+++ src/lib.rs
@@ -161,9 +161,10 @@ pub extern "C" fn get_all_doc_count() -> i64 {
 pub extern "C" fn split_sub_objects_to_db (json_str: *const c_char) -> i64 {
     let doc = c_str_to_bson(json_str);
     let key = String::from("sub_objects");
-    let vec_iter = doc.get_array(&key).unwrap().to_owned().iter();
-    let array = vec_iter
-        .map(|bson :Bson| bson.as_document().unwrap().to_owned());
+    let bson_vec: Vec<Bson> = doc.get_array(&key).unwrap().to_owned();
+    
+    let array = bson_vec.iter()
+        .map(|bson :Bson| bson.as_document().unwrap().to_owned()).collect();
     MONGO_COLL.insert_many(array, None)
     .ok().expect("Failed to insert document.");
     array.len() as i64
